ff621671cd6805176e594a490d06af0a0eb40592
Hubertzhang <hubert@Huberts-MacBook-Pro.local>
2013-10-14 22:35:51 +0800

    Small Addtion of Storing  cards

--- server.h
+++ server.h
@@ -5,6 +5,8 @@
 #include <QObject>
 #include <cstring>
 #include <exception>
+#define CARD_NUMBER 150
+
 struct Team
 {
     int grail[2];
@@ -16,13 +18,21 @@ class Server : public QObject
 {
     Q_OBJECT
     static int PlayerNumber;
-    Card card[];
+    Card card[CARD_NUMBER];
     Player* players;
     Team team[2];
     void shuffle_cards();
     void deal_cards(int id,int number);
     void init();
     void playerturn(int t);
+
+    int* Pile;
+    int* DiscardPile;
+    int* BeginOfPile;                       //current card
+    int* EndOfPile;                         //
+    int* BeginOfDiscardPile;                //
+    int* EndOfDiscardPile;                  //place the discard (point to an empty slot)
+
 public:
     explicit Server(QObject *parent = 0, int Number=4);
 
