b933bd60d6f9f5c8c4d54d22c7c9e351bec2d560
FarewellX <610499190@qq.com>
2013-11-01 21:52:19 +0800

    conflict fixed

--- cardList.h
+++ cardList.h
@@ -5,11 +5,7 @@
 class CardList
 {
     Q_OBJECT
-<<<<<<< HEAD
     friend class Player;
-=======
-    friend class player;
->>>>>>> 2b8d22289801164e8c34f1f9235efe33d45e59e4
 private:
     Card cardList[150];
 public:
